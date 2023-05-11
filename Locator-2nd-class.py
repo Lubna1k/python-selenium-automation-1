ac8adf0758c0889e266d5383a1c536fc4b3b7e7e
Lubna Khan <lubna1khan39@yahoo.com>
2023-05-10 22:14:36 -0400

    Locator 2nd class

--- Locator-2nd-class.py
+++ Locator-2nd-class.py
@@ -13,30 +13,40 @@ driver = webdriver.Chrome(service=service)
 driver.maximize_window()
 
 # open the url
-
 driver.get('https://www.amazon.com/')
-sleep(4)
-driver.find_element(By.CLASS_NAME, 'a-icon a-icon-logo')
-driver.find_elements(By.XPATH, "//a[@href='/ref=nav_logo")
-driver.find_element(By.XPATH, "//a")
-driver.find_element(By.XPATH, "//*[@href='/ref=nav_logo")
-driver.find_element(By.XPATH, "//*[@href='/ref=nav_logo' and @aria-label='Amazon']")
+
+driver.find_element(By.ID, 'nav-orders').click()
+
+#Amazon logo
+#driver.find_element(By.CLASS_NAME, "a-icon a-icon-logo")
+#driver.find_element(By.XPATH, "//input[@aria-label='Amazon logo']")
+#SIGN IN
+#driver.find_element(By.CLASS_NAME, "a-spacing-small")
+#Continue button
+#driver.find_element(By.CLASS_NAME, 'a-icon a-icon-logo')
+
+# Amazon Logo
+driver.find_elements(By.XPATH, "//a[@href='/ref=ap_frn_logo']")
+
+#sign in
+driver.find_element(By.CLASS_NAME,'a-spacing-small')
 #driver.find_element(By.XPATH, "//a[contains(@href, 'nav_cs_bestsellers')]")
 
 driver.find_element(By.CLASS_NAME, "a-expander-prompt")
-driver.find_element(By.CLASS_NAME, "Need help?")
-driver.find_element(By.XPATH, "//span[contains(text(), 'Need')]")
+
+#need hel
+driver.find_element(By.XPATH, '//span[contains(text(), " Need help?")]')
 #Forgot your password
 driver.find_element(By.ID, "auth-fpp-link-bottom")
 #sign in link
 driver.find_element(By.ID, "ap-other-signin-issues-link")
 
 ####create amazon account
-driver.find_element(By.CSS_SELECTOR, 'createAccountSubmit')
+driver.find_element(By.ID, 'createAccountSubmit')
 
 ##conditions of use link
-driver.find_element(By.CSS_SELECTOR, 'a*[href="condition_of_use"]')
+driver.find_element(By.CSS_SELECTOR, 'a[href*="condition_of_use"]')
 
-#####privacy notice link
+###privacy notice link
 driver.find_element(By.CSS_SELECTOR, 'a[href*="notification_privacy_notice"]')
 driver.quit()
